ee0015cb6f2199f8cba3e73d0fadbed7c519efc8
kanyun <949955482@qq.com>
2017-09-10 17:35:59 +0800

    ng2初步

--- cpa-web/src/main/java/cn/kanyun/cpa/controller/itempool/CpaSolutionController.java
+++ cpa-web/src/main/java/cn/kanyun/cpa/controller/itempool/CpaSolutionController.java
@@ -4,9 +4,11 @@ import cn.kanyun.cpa.model.dto.itempool.CpaRepertoryDto;
 import cn.kanyun.cpa.model.entity.CpaResult;
 import cn.kanyun.cpa.service.itempool.ICpaSolutionService;
 import cn.kanyun.cpa.service.itempool.impl.CpaSolutionServiceImpl;
+import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -19,29 +21,35 @@ import java.util.Map;
 /**
  * Created by KANYUN on 2017/6/17.
  */
-@Controller("/api/solution")
+@Controller
+@RequestMapping("/api/solution")
 public class CpaSolutionController {
 
     private static final Logger logger = LoggerFactory.getLogger(CpaRepertoryController.class);
 
     @Resource
     private ICpaSolutionService cpaSolutionService;
+
     /**
-     *@Author: kanyun
-     *@Description: 检查试题答案
-     *@Date: 2017/8/16 15:03
-     *@params:
+     * @Author: kanyun
+     * @Description: 检查试题答案
+     * @Date: 2017/8/16 15:03
+     * @params:
      */
     @RequestMapping("/correctItem")
     @ResponseBody
-    public CpaResult correctItem(List<Map<Integer,String>> list){
-        Map<Integer,String> peopleAnswer = new HashMap<>();
-        Iterator iterator = list.iterator();
-        while (iterator.hasNext()){
-            Map.Entry<Integer,String> entry = (Map.Entry) iterator.next();
-            peopleAnswer.put(entry.getKey(),entry.getValue());
+/*    @RequestBody接收的是一个Json对象的字符串，而不是一个Json对象。然而在ajax请求往往传的都是Json对象，后来发现用 JSON.stringify(data)的方式就能将对象变成字符串。
+    同时ajax请求的时候也要指定dataType: "json",contentType:"application/json" 这样就可以轻易的将一个对象或者List传到Java端，使用@RequestBody即可绑定对象或者List*/
+    public CpaResult correctItem(@RequestBody List<String> pAnswers) {
+        Map<Integer, String> peopleAnswer = new HashMap<>();
+        Iterator iterator = pAnswers.iterator();
+        while (iterator.hasNext()) {
+            String str = (String) iterator.next();
+            String[] strr = str.split("-");
+            peopleAnswer.put(Integer.valueOf(strr[0]),strr[1]);
         }
         CpaResult result = cpaSolutionService.compareAnswer(peopleAnswer);
         return result;
     }
+
 }
