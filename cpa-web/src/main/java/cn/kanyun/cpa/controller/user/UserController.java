5943aaba5bb326fe6874781b960236d99c903a30
kanyun <949955482@qq.com>
2018-03-23 20:04:46 +0800

    加入lucene

--- cpa-web/src/main/java/cn/kanyun/cpa/controller/user/UserController.java
+++ cpa-web/src/main/java/cn/kanyun/cpa/controller/user/UserController.java
@@ -19,10 +19,7 @@ import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
