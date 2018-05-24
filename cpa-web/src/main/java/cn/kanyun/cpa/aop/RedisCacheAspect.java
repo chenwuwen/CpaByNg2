27238df94c3acbd65979cd2c196e71d5817e4633
“kanyun <2501951819@qq.com>
2018-05-24 19:39:33 +0800

    加入lucene

---
+++ cpa-web/src/main/java/cn/kanyun/cpa/aop/RedisCacheAspect.java
@@ -0,0 +1,128 @@
+package cn.kanyun.cpa.aop;
+
+import cn.kanyun.cpa.dao.common.annotation.RedisCacheProfiler;
+import cn.kanyun.cpa.redis.JedisClusterFactory;
+import com.alibaba.fastjson.JSONObject;
+import javassist.bytecode.SignatureAttribute;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by kanyun on 2018/5/24 0024.
+ * Redis缓存注解AOP实现
+ */
+@Component
+@Aspect
+public class RedisCacheAspect {
+
+    private static volatile JedisCluster jedisCluster = null;
+
+    @Value("${redis.host}")
+    private static String host;
+    @Value("${redis.port}")
+    private static String port;
+    @Value("${redis.password}")
+    private static String password;
+
+    public static JedisCluster getJedisCluster() {
+        if (jedisCluster == null) {
+            synchronized (JedisCluster.class) {
+                if (jedisCluster == null) {
+                    jedisCluster = new JedisCluster();
+                }
+            }
+        }
+        return jedisCluster;
+    }
+
+
+    @Pointcut("@annotation(cn.kanyun.cpa.dao.common.annotation.RedisCacheProfiler)")
+
+    public void aspectjMethod() {
+
+    }
+
+
+    @Around("aspectjMethod()")
+    public Object doAround(ProceedingJoinPoint pjp) throws Throwable {
+
+        Object obj = null;
+        try {
+            //获取方法
+            Method method = this.getMethod(pjp);
+            RedisCacheProfiler redisCacheProfiler = method.getAnnotation(RedisCacheProfiler.class);
+
+            //方法上没有注解，直接执行方法然后返回
+            if (null == redisCacheProfiler) {
+                return pjp.proceed();
+            }
+
+            //缓存key
+            String cacheKey = this.getCacheKey(pjp, redisCacheProfiler);
+
+            //true：从缓存读
+            if (redisCacheProfiler.readFromCache()) {
+                obj = jedisCluster.get(cacheKey);
+            } else {
+                return pjp.proceed();
+            }
+            if (null == obj) {
+                obj = pjp.proceed();
+            } else {
+                return obj;
+            }
+            if (null != obj) {
+                jedisCluster.setnx(cacheKey, JSONObject.toJSONString(obj));
+            }
+            return obj;
+
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        return obj;
+    }
+
+    /**
+     * @describe: 获取方法
+     * @params:
+     * @Author: Kanyun
+     * @Date: 2018/5/24 0024 17:15
+     */
+    private Method getMethod(JoinPoint joinPoint) {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Method method = methodSignature.getMethod();
+        return method;
+    }
+
+    /**
+     * @describe: 获取缓存key的方法
+     * @params:
+     * @Author: Kanyun
+     * @Date: 2018/5/24 0024 17:22
+     */
+    private String getCacheKey(ProceedingJoinPoint pjp, RedisCacheProfiler redisCacheProfiler) {
+        StringBuffer sb = new StringBuffer();
+        if (pjp.getArgs() != null && pjp.getArgs().length != 0) {
+            Object[] arr = pjp.getArgs();
+            for (Object o : arr) {
+                if (null != o) {
+                    sb.append("_").append(String.valueOf(o));
+                }
+            }
+            return sb.toString();
+        } else {
+            return sb.toString();
+        }
+    }
+
+}
