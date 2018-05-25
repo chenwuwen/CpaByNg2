c68a9ea4c685174a01d3f30f43527a3dc47fd00b
“kanyun <2501951819@qq.com>
2018-05-25 19:15:01 +0800

    加入lucene

--- cpa-web/src/main/java/cn/kanyun/cpa/listenner/TomcatListener.java
+++ cpa-web/src/main/java/cn/kanyun/cpa/listenner/TomcatListener.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebListener;
 public class TomcatListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("当前工作目录路径为：" + sce.getServletContext().getRealPath("/"));
         System.out.println("tomcat关闭了..........");
     }
 
